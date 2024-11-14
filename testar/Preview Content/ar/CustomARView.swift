573d7af66cf9ffe4c895b4807ef4f70ed4bb28c4
soda <114852162+s1223takuma@users.noreply.github.com>
2024-11-14 12:50:06 +0900

    ARの素材をメタリックに

--- testar/Preview Content/ar/CustomARView.swift
+++ testar/Preview Content/ar/CustomARView.swift
@@ -21,23 +21,47 @@ class ARViewController: UIViewController {
         // この構成でセッションを実行すると、フロントカメラに切り替わります。
 
         let anchor = AnchorEntity(.face)
-        let text = ModelEntity(mesh: .generateText(" ←", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
-//        let text1 = ModelEntity(mesh: .generateText(" ←1", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
-//        let text2 = ModelEntity(mesh: .generateText(" ←2", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
-//        let text3 = ModelEntity(mesh: .generateText(" ←3", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
-         // "Ciao!"という文字列、奥行き0.03m、システムフォント0.1m-bold、テキストの折り返しbyCharWrappingでモデルを作成。
+        let text = ModelEntity(mesh: .generateText(" ←0", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
+        let material = SimpleMaterial(color: .green,
+                                      roughness: 0, // 表面の粗さ
+                                      isMetallic: true) // 光を反射するか
+         // 青色、粗さ0、メタリックのシンプルなマテリアルをつくる
+
+        text.model?.materials = [material] // ボックスの表面に適用する
+        let text1 = ModelEntity(mesh: .generateText(" ←1", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
+        let material1 = SimpleMaterial(color: .red,
+                                      roughness: 0, // 表面の粗さ
+                                      isMetallic: true) // 光を反射するか
+         // 青色、粗さ0、メタリックのシンプルなマテリアルをつくる
+
+        text1.model?.materials = [material1] // ボックスの表面に適用する
+        let text2 = ModelEntity(mesh: .generateText(" ←2", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
+        let material2 = SimpleMaterial(color: .white,
+                                      roughness: 0, // 表面の粗さ
+                                      isMetallic: true) // 光を反射するか
+         // 青色、粗さ0、メタリックのシンプルなマテリアルをつくる
+
+        text2.model?.materials = [material2] // ボックスの表面に適用する
+        let text3 = ModelEntity(mesh: .generateText(" ←3", extrusionDepth: 0.01, font: .systemFont(ofSize: 0.2, weight: .bold), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping))
+        let material3 = SimpleMaterial(color: .blue,
+                                      roughness: 0, // 表面の粗さ
+                                      isMetallic: true) // 光を反射するか
+         // 青色、粗さ0、メタリックのシンプルなマテリアルをつくる
+
+        text3.model?.materials = [material3] // ボックスの表面に適用する
+         // "←"という文字列、奥行き0.03m、システムフォント0.1m-bold、テキストの折り返しbyCharWrappingでモデルを作成。
          // containerFrameをゼロにすると、その文字列の表示に必要な大きさに文字列のコンテナが調整される。
          // コンテナフレームはモデルの原点（中心）にoriginが設定されるので、origin0だと少し右寄りになる。
 
          // フォントサイズは、m単位になるので、20にすると20mで表示されてしまうので注意。
         text.transform = Transform(pitch: 0, yaw: 0, roll: 0)
-//        text1.transform = Transform(pitch: 0, yaw: 0, roll: -1.5707963267949)
-//        text2.transform = Transform(pitch: 0, yaw: -1.5707963267949, roll: 0)
-//        text3.transform = Transform(pitch: -1.5707963267949, yaw: 0, roll: 0)
+        text1.transform = Transform(pitch: 0, yaw: 0, roll: -1.5707963267949)
+        text2.transform = Transform(pitch: 0, yaw: -1.5707963267949, roll: 0)
+        text3.transform = Transform(pitch: -1.5707963267949, yaw: 0, roll: 0)
         anchor.addChild(text)
-//        anchor.addChild(text1)
-//        anchor.addChild(text2)
-//        anchor.addChild(text3)
+        anchor.addChild(text1)
+        anchor.addChild(text2)
+        anchor.addChild(text3)
         arView.scene.anchors.append(anchor)
 
 
