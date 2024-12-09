04e7c6c60369f84e21f4e39d026f574291371f62
soda <114852162+s1223takuma@users.noreply.github.com>
2024-12-09 11:22:18 +0900

    再コミット

---
+++ testar/Preview Content/timeline/Post.swift
@@ -0,0 +1,22 @@
+//
+//  post.swift
+//  testar
+//
+//  Created by 関琢磨 on 2024/11/12.
+//
+
+import SwiftUI
+
+struct Post: Identifiable {
+    let id = UUID()
+    let username: String
+    let handle: String
+    let avatarName: String
+    let content: String
+    let timeAgo: String
+    var comments: Int
+    var retweets: Int
+    var likes: Int
+    var islikes: Bool
+    var isretweets: Bool
+}
